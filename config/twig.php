63bb85e5033d4511f7b93a772bcfc2983f39f0a6
Angelo Lauricella <angelo_lauricella@icloud.com>
2023-04-26 13:09:00 +0200

    quest

--- config/twig.php
+++ config/twig.php
@@ -1,7 +1,16 @@
 <?php
 
+
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
+
+//On ajoute la globale author à l'initialisation de l'objet Twig
+$author = 'ANGELO Wilder';
+$contactEmail = 'angelo_lauricella@icloud.com';
+$twig->addGlobal('author', $author );
+$twig->addGlobal('contactEmail' ,$contactEmail);
+
+
